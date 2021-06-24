f3f9475e0784fa0b809a9d3964b77b6a4e68a1a9
Sibyl <mcanjelo29@gmail.com>
2021-06-24 19:47:45 +0800

    Remove duplicate expanded widget on category selection

--- lib/screens/categorySelectionTab.dart
+++ lib/screens/categorySelectionTab.dart
@@ -19,38 +19,36 @@ class _CategorySelectionTabState extends State<CategorySelectionTab> {
   Widget build(BuildContext context) {
     final user = context.watch<User>();
 
-    return Expanded(
-      child: GridView.count(
-        padding: EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 0.0),
-        crossAxisCount: 4,
-        mainAxisSpacing: 8.0,
-        childAspectRatio: 8/9,
-        children: widget.categoryType == CategoryType.expense ? user.expenseCategories.map((e) => CategoryButton(
-            color: Color(e.color).withOpacity(1),
-            icon: IconData(e.icon, fontFamily: 'MaterialIcons'),
-            name: e.name,
-            categoryID: e.categoryID,
-            value : user.getCategoryNet(month: DateTime.now().month, year: DateTime.now().year, categoryID: e.categoryID),
-            currencySymbol: user.primaryCurrency.symbol,
-            onCategoryClick: (int categoryID) {
-              Navigator.pop(context, {
-                "categoryID": categoryID,
-              });
-            }
-        )).toList() : user.incomeCategories.map((e) => CategoryButton(
-            color: Color(e.color).withOpacity(1),
-            icon: IconData(e.icon, fontFamily: 'MaterialIcons'),
-            name: e.name,
-            categoryID: e.categoryID,
-            value : user.getCategoryNet(month: DateTime.now().month, year: DateTime.now().year, categoryID: e.categoryID),
-            currencySymbol: user.primaryCurrency.symbol,
-            onCategoryClick: (int categoryID) {
-              Navigator.pop(context, {
-                "categoryID": categoryID,
-              });
-            }
-        )).toList(),
-      ),
+    return GridView.count(
+      padding: EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 0.0),
+      crossAxisCount: 4,
+      mainAxisSpacing: 8.0,
+      childAspectRatio: 8/9,
+      children: widget.categoryType == CategoryType.expense ? user.expenseCategories.map((e) => CategoryButton(
+          color: Color(e.color).withOpacity(1),
+          icon: IconData(e.icon, fontFamily: 'MaterialIcons'),
+          name: e.name,
+          categoryID: e.categoryID,
+          value : user.getCategoryNet(month: DateTime.now().month, year: DateTime.now().year, categoryID: e.categoryID),
+          currencySymbol: user.primaryCurrency.symbol,
+          onCategoryClick: (int categoryID) {
+            Navigator.pop(context, {
+              "categoryID": categoryID,
+            });
+          }
+      )).toList() : user.incomeCategories.map((e) => CategoryButton(
+          color: Color(e.color).withOpacity(1),
+          icon: IconData(e.icon, fontFamily: 'MaterialIcons'),
+          name: e.name,
+          categoryID: e.categoryID,
+          value : user.getCategoryNet(month: DateTime.now().month, year: DateTime.now().year, categoryID: e.categoryID),
+          currencySymbol: user.primaryCurrency.symbol,
+          onCategoryClick: (int categoryID) {
+            Navigator.pop(context, {
+              "categoryID": categoryID,
+            });
+          }
+      )).toList(),
     );
   }
 }
