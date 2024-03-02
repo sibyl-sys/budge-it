39cc1c14563bf6362e095a51c70e6230de7b8f5e
mcanjelo <mcanjelo1n@ai-work.com>
2024-03-02 20:14:45 +0800

    Update header text for currency selection

--- lib/screens/currencySelection.dart
+++ lib/screens/currencySelection.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:money_tracker/constants/Constants.dart';
 
-
 class CurrencySelection extends StatefulWidget {
   const CurrencySelection({Key? key}) : super(key: key);
 
@@ -20,71 +19,66 @@ class _CurrencySelectionState extends State<CurrencySelection> {
           height: 475,
           decoration: BoxDecoration(
               color: Colors.white,
-              borderRadius: BorderRadius.all(Radius.circular(15))
-          ),
-          child:  Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(24.0),
-                    child: SizedBox(
-                      height: 25,
-                      child: Center(
-                        child: Text(
-                          "Select Account Icon",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            fontSize: 18,
-                          ),
-                        ),
+              borderRadius: BorderRadius.all(Radius.circular(15))),
+          child: Column(children: [
+            Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: SizedBox(
+                  height: 25,
+                  child: Center(
+                    child: Text(
+                      "Select Currency",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 18,
                       ),
-                    )
+                    ),
                   ),
-                  Expanded(
-                    child: ListView(
-                      padding: EdgeInsets.zero,
-                      children: currencyList.map((currency) =>
-                          Material(
+                )),
+            Expanded(
+              child: ListView(
+                  padding: EdgeInsets.zero,
+                  children: currencyList
+                      .map((currency) => Material(
                             color: Colors.white.withOpacity(0),
                             child: InkWell(
                               onTap: () {
                                 Navigator.pop(context, currency);
                               },
-                              splashColor: Theme.of(context).primaryColor.withOpacity(0.7),
+                              splashColor: Theme.of(context)
+                                  .primaryColor
+                                  .withOpacity(0.7),
                               child: Container(
-                                padding: EdgeInsets.fromLTRB(24.0, 8.0, 24.0, 8.0),
+                                padding:
+                                    EdgeInsets.fromLTRB(24.0, 8.0, 24.0, 8.0),
                                 child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
                                   children: [
                                     Flexible(
                                       child: Text(
                                         currency.name,
                                         style: TextStyle(
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.w400
-                                        ),
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w400),
                                       ),
                                     ),
-                                    Text(
-                                      currency.symbol,
-                                      style: TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.w500,
-                                        fontFamily: 'Roboto'
-                                      )
-                                    )
+                                    Text(currency.symbol,
+                                        style: TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w500,
+                                            fontFamily: 'Roboto'))
                                   ],
                                 ),
                               ),
                             ),
-                          )
-                      ).toList()
-                    ),
-                  ),
-                  SizedBox(height: 16.0)
-                ]
+                          ))
+                      .toList()),
             ),
-          ),
+            SizedBox(height: 16.0)
+          ]),
         ),
+      ),
     );
   }
 }
